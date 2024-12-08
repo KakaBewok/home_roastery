09c3c361058060f3aece4e5a409b8152f5e5435c
noprizal <rizalnov667@gmail.com>
2024-12-08 22:00:44 +0700

    setup order

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -9,6 +9,7 @@ use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Filters\Filter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -17,7 +18,12 @@ class OrderResource extends Resource
 {
     protected static ?string $model = Order::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-percent-badge';
+
+    public static function canCreate(): bool
+    {
+        return false;
+    }
 
     public static function form(Form $form): Form
     {
@@ -31,13 +37,38 @@ class OrderResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('order_date')->dateTime('Y-m-d H:i')->sortable(),
+                Tables\Columns\TextColumn::make('user.name')->label('Customer')->searchable(),
+                Tables\Columns\TextColumn::make('status')
+                    ->badge()
+                    ->label('Status')
+                    ->color(fn(string $state): string => match ($state) {
+                        'Pending' => 'warning',
+                        'Paid' => 'success',
+                        'Cancelled' => 'danger',
+                    }),
+                Tables\Columns\TextColumn::make('total_amount')
+                    ->formatStateUsing(fn($state) => 'Rp. ' . number_format($state, 0, ',', '.'))->sortable(),
             ])
             ->filters([
-                //
+                Filter::make('status')
+                    ->label('Order Status')
+                    ->query(
+                        fn(Builder $query, $data): Builder =>
+                        $query->when($data['value'], fn($query, $status) => $query->where('status', '=', $status))
+                    )
+                    ->form([
+                        Forms\Components\Select::make('value')->label('Order status')
+                            ->options([
+                                'Pending' => 'Pending',
+                                'Paid' => 'Paid',
+                                'Cancelled' => 'Cancelled',
+                            ])
+                            ->placeholder('Select order status'),
+                    ]),
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
